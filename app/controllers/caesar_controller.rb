c36f24ecdfa9016fa1c9cb64f86e9d4013ff6086
TheBlackArroVV <theblackarrovv@gmail.com>
2018-03-06 15:35:31 +0200

    i still playing with os

--- app/controllers/caesar_controller.rb
+++ app/controllers/caesar_controller.rb
@@ -1,9 +1,11 @@
 class CaesarController < ApplicationController
   def index
     @caesar = Caesar.new
-    @caesar.word = params[:caesar][:word]
-    @caesar.count = params[:caesar][:count]
-    @caesar.caesar
+    if !params[:caesar].nil?
+      @caesar.word = params[:caesar][:word]
+      @caesar.count = params[:caesar][:count]
+      @caesar.caesar
+    end
   end
 
   def create
--- app/models/caesar.rb
+++ app/models/caesar.rb
@@ -2,7 +2,11 @@ class Caesar < ApplicationRecord
   attr_accessor :word
   attr_accessor :count
 
+  ALPHABET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
   def caesar
+    self.word = self.word.split("")
+    self.word.map! { |letter| letter =  ALPHABET[(ALPHABET.index(letter)) + self.count] }
     pp self.word
   end
 end
