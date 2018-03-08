0bc64078f9eae4d064d2fb3274c62627f2713036
TheBlackArroVV <theblackarrovv@gmail.com>
2018-03-08 23:20:22 +0200

    caesar encrypt and decrypt

--- app/controllers/caesar_controller.rb
+++ app/controllers/caesar_controller.rb
@@ -1,11 +1,30 @@
 class CaesarController < ApplicationController
   def index
     @caesar = Caesar.new
-    @caesar.word = params[:caesar][:word]
-    @caesar.count = params[:caesar][:count]
-    @caesar.caesar
   end
 
   def create
+    @caesar = Caesar.new
+    if @caesar.save
+      @caesar = Caesar.new(caesar_params)
+      @result = @caesar.caesar
+      render 'index'
+    end
+  end
+
+  def decrypt
+    @caesar = Caesar.new
+    # if @caesar.save
+      @caesar = Caesar.new(caesar_params)
+      pp @caesar.word
+      pp @result = @caesar.brutus
+      # render 'index'
+    # end
+  end
+
+  private
+
+  def caesar_params
+    params.require(:caesar).permit(:word, :count)
   end
 end
